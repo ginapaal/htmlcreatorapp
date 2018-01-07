1403115172757ca586bddb9af1aa3235447b3daa
Gina <paalginaa@gmail.com>
2018-01-07 22:16:58 +0100

    jquery send picture data to server

--- src/main/java/creator/HTMLCreator.java
+++ src/main/java/creator/HTMLCreator.java
@@ -1,10 +1,13 @@
 package creator;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HTMLCreator {
 
     public static void main(String[] args) {
-         ImageHandler imageHandler= new ImageHandler();
-
-         imageHandler.getImageFromClipboard();
+        SpringApplication.run(HTMLCreator.class, args);
     }
 }
---
+++ src/main/java/creator/picture/controller/PictureController.java
@@ -0,0 +1,43 @@
+package creator.picture.controller;
+
+
+import creator.picture.ImageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PictureController {
+
+    @Autowired
+    ImageHandler imageHandler;
+
+    List<String> htmlData = new ArrayList<>();
+
+    @GetMapping(value = "/")
+    public String render() {
+        return "textarea";
+    }
+
+
+    @PostMapping(value = "/")
+    public String gettingData (@RequestParam("images") String imageContent){
+        imageHandler.getImageURL(imageContent);
+        List<String> urls = imageHandler.getUrls();
+        htmlData.addAll(urls);
+        return "redirect:/";
+    }
+
+    @GetMapping(value="/data")
+    public String renderURLs(Model model) {
+        model.addAttribute("data", htmlData);
+        return "response";
+    }
+
+}
